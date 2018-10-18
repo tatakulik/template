7582bd96c61cfe53e309f2c9c25c5b6dda4c965f
Tatiana Karaseva <tatak@yandex.ru>
2018-10-18 10:43:06 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+// @flow
+
+import React, { Component } from 'react';
+import './App.css';
+
+type PropsType = {};
+
+class App extends Component<PropsType> {
+  render() {
+    return (
+      <div className="App">
+        132
+      </div>
+    );
+  }
+}
+
+export default App;
