5f64942aeb748561b10ade8ef6c1a561a242dd50
Tatiana Karaseva <tatak@yandex.ru>
2018-11-23 14:02:00 +0300

    в процессе addtodo

--- src/Addtodo.js
+++ src/Addtodo.js
@@ -2,11 +2,30 @@
 
 import React, { Component } from 'react';
 import './Addtodo.css';
+import Todolist from './Todolist';
 
-type PropsType = { onClick: () => void };
-// type StateType = { box: string };
+type PropsType = { onClick: () => void, list: Array<{checked: boolean, task: string}>,   };
+type StateType = { box: string };
 
 class Addtodo extends Component<PropsType> {
+    constructor(props) {
+        super(props);
+        this.state={ box: "" }
+        this.updateState=this.updateState.bind(this);
+    };
+    updateState() {
+        this.setState({box: string})
+    };
+    taskPlus(box: string) {
+        const Plus=[...]
+    }
+
+
+};
+
+
+
+
   render() {
     return (
       <div className="Addtodo">
--- src/Todolist.js
+++ src/Todolist.js
@@ -2,19 +2,23 @@
 
 import React, { Component } from 'react';
 import './Todolist.css';
+import Todo from './Todo';
 
 
-type PropsType = { list: array<{  }> };
+type PropsType = { list: Array<{checked: boolean, task: string}> };
 
 class Todolist extends Component<PropsType> {
   render() {
     return (
-      <div className="App">
-        <Todo task="clean" />
-        <Addtodo />
+      <div className="Todolist">
+        {this.props.list.map((elem, i) => <Todo key={i} task={elem.task} />)}
       </div>
     );
   }
 }
+// let task = {
+//     checked: false,
+//     task: "clean"
+// }
 
 export default Todolist;
